d4ef66d7c997cdcc85188f2fed93da97beea06b1
seanmisra <seanmisra30@gmail.com>
2023-04-26 21:13:37 -0400

    Exercise 5: POST API with body, logic, error handling

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Commits correspond to each exercises
 2. Web server with http module
 3. Web server with express module
 4. Configure CORS with express (allow all origins)
+5. Write POST API that returns sum of all elements in array (make sure to handle error scenarios)
--- app.js
+++ app.js
@@ -10,9 +10,40 @@ app.use(
     })
 );
 
+app.use(
+    express.json()
+);
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+app.post("/sum", (req, res) => {
+    const input = req.body;
+
+    if (!input || (!Array.isArray(input) && input.size === undefined)) {
+        res.status(400).send({
+            message: "Input must be an array of integers"
+        });
+        return;
+    } else if (input.length === 0) {
+        res.status(400).send({
+            message: "Array must include at least one integer"
+        });
+        return;
+    }
+
+    const allNumbers = input.every(x => !isNaN(parseInt(x)))
+    if (!allNumbers) {
+        res.status(400).send({
+            message: "Not all inputs are numbers"
+        })
+        return;
+    }
+
+    const sum = input.reduce((nextInt, total) => nextInt + total);
+    return res.send({sum: sum});
+})
 
 app.listen(PORT, HOST, () => {
     console.log(`Listening at http://${HOST}:${PORT}`);
