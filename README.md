0fd7e064e9ab922f365f0264bf37101efdf9bf73
seanmisra <seanmisra30@gmail.com>
2023-04-26 20:45:56 -0400

    Exercise 4: configure CORS

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Commits correspond to each exercises
 1. Setup npm project
 2. Web server with http module
 3. Web server with express module
+4. Configure CORS with express (allow all origins)
--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const PORT = 3000;
 const HOST = 'localhost';
 const app = express();
+
+app.use(
+    cors({
+        origin: "*"
+    })
+);
 
 app.get("/", (req, res) => {
     res.send("Hello World");
